f151bd7fba696164b5086731919e3ab457c30fe1
ShyperOne <syperpolsovatel@mail.ru>
2020-04-24 08:34:01 +0300

    lesson-14

--- Snake_origin/Snake_origin/Program.cs
+++ Snake_origin/Snake_origin/Program.cs
@@ -15,25 +15,42 @@ namespace snake
         }
         static void Main(string[] args)
         {
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '$');
-            Draw(v1);
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
             Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-            foreach(var f in figures)
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
             {
-                f.Draw();
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
             }
-
-
-            Console.ReadLine();
         }
     }
 }
